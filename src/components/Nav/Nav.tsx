bc9a5eefd2e7f6b53c650e3b7b5224a29b9d8ccd
Andy Wright <atwright147@gmail.com>
2023-03-01 20:24:28 +0000

    update nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -4,13 +4,13 @@ import styles from './Nav.module.scss';
 
 export const Nav = (): JSX.Element => (
   <nav className={styles.nav} aria-label="Primary">
+    <h2>Routes</h2>
     <NavLink className={styles.link} to="/">Home</NavLink>
     <NavLink className={styles.link} to="/drag-and-drop">Drag and Drop</NavLink>
-    <NavLink className={styles.link} to="/articles">Articles</NavLink>
-    <NavLink className={styles.link} to="/issues">Issues</NavLink>
     <NavLink className={styles.link} to="/todos">Todos</NavLink>
     <NavLink className={styles.link} to="/starwars">Star Wars</NavLink>
     <hr />
+    <h2>Components</h2>
     <NavLink className={styles.link} to="/components/accordion">Accordion</NavLink>
     <NavLink className={styles.link} to="/components/button">Button</NavLink>
     <NavLink className={styles.link} to="/components/fields">Fields</NavLink>
