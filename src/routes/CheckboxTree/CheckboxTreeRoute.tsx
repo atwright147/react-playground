32888aeabf7b6e11f083da8afc3373df44ea951f
Andy Wright <atwright147@gmail.com>
2024-08-01 21:23:31 +0100

    update checkbox tree layout

--- src/routes/CheckboxTree/CheckboxTreeRoute.tsx
+++ src/routes/CheckboxTree/CheckboxTreeRoute.tsx
@@ -3,12 +3,14 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { CheckboxTree, type Item } from '../../components/CheckboxTree/CheckboxTree';
 
+faker.seed(42);
+
 const items: Item[] = [];
 
-for (let parentIndex = 1; parentIndex <= 10; parentIndex++) {
+for (let parentIndex = 1; parentIndex <= 5; parentIndex++) {
   const children: Item[] = [];
 
-  for (let childIndex = 1; childIndex <= 3; childIndex++) {
+  for (let childIndex = 1; childIndex <= faker.number.int({ min: 1, max: 5 }); childIndex++) {
     children.push({
       id: uuidv4(),
       name: faker.lorem.words(3),
