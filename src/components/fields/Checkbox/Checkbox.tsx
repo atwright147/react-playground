a938e0477fe5b99b4cbe88de0c8a81aac9377f0c
Andy Wright <atwright147@gmail.com>
2024-08-01 14:48:47 +0100

    handle indeterminate state for checkboxes

--- src/components/fields/Checkbox/Checkbox.tsx
+++ src/components/fields/Checkbox/Checkbox.tsx
@@ -1,7 +1,7 @@
 import classnames from 'classnames';
-import { type ComponentPropsWithRef, type FC, forwardRef, type ReactNode, type Ref } from 'react';
-import styles from './Checkbox.module.scss';
+import { type ComponentPropsWithRef, type FC, type ReactNode, type Ref, forwardRef } from 'react';
 import { Messages } from '../Messages/Messages';
+import styles from './Checkbox.module.scss';
 
 // https://stackoverflow.com/a/66810748/633056
 
@@ -12,18 +12,28 @@ export interface Props extends Omit<ComponentPropsWithRef<'input'>, 'name' | 'id
   left?: ReactNode;
   middle?: ReactNode;
   right?: ReactNode;
+  // biome-ignore lint/suspicious/noExplicitAny: fix later
   errors?: any;
+  indeterminate?: boolean;
 }
 
 export const Checkbox: FC<Props> = forwardRef<HTMLInputElement, Props>(
-  ({ label, name, id, className, errors, ...props }, ref: Ref<HTMLInputElement>): JSX.Element => {
+  ({ indeterminate = false, label, name, id, className, errors, ...props }, ref: Ref<HTMLInputElement>): JSX.Element => {
     const describedby = `${id}--errors`;
 
     return (
       <div className={classnames(styles.field, className)}>
         <label htmlFor={id} className={styles.label}>
-          <input id={id} className={styles.input} type="checkbox" aria-describedby={describedby} ref={ref} {...props} />
-          <span className={styles.checkmark}></span>
+          <input
+            id={id}
+            className={styles.input}
+            type="checkbox"
+            aria-describedby={describedby}
+            ref={ref}
+            {...props}
+            data-indeterminate={indeterminate}
+          />
+          <span className={styles.checkmark} />
           {label}
         </label>
         <Messages messages={errors} id={describedby} />
